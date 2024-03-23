8c2aa129e1419192a19c51f97f4505cb21634fd7
AdRimss <akhmadkarim1234@gmail.com>
2024-03-23 15:13:10 +0700

    quiz 9.4

---
+++ minggu3/quizlogic-9.4/soal4.js
@@ -0,0 +1,37 @@
+/*
+Diberikan sebuah function checkAB(str) yang menerima satu parameter berupa string. function tersebut mengembalikan nilai true jika di dalam string tersebut terdapat karakter a dan b yang memiliki jarak 3 karakter lain minimal satu kali. Jika tidak ditemukan sama sekali, kembalikan nilai false. Jarak bisa dari a ke b, atau b ke a.
+
+contoh:
+barbarian kenapa bisa true?
+karena di bagian 'barian' terdapat b dipisah 3 karakter ari(totalnya 3) dan ketemu a
+
+Spasi juga dianggap sebagai karakter
+
+- terdapat karakter a dan b //Jika ada a Nested Loop Cari B -> Lakukan Pengecekan
+- Spasi juga dianggap sebagai karakter
+- memiliki jarak 3 //4
+- Jarak bisa dari a ke b, atau b ke a. //Math.abs
+- Jika tidak ditemukan sama sekali, kembalikan nilai false. 
+*/
+
+function checkAB(num) {
+    // you can only write your code here!
+    let value = false
+    for(let i = 0; i < num.length; i++){
+        if(num[i] === 'a'){
+            for(let j = 0; j < num.length; j++){
+                if(num[j] === 'b' && Math.abs(i-j) == 4){
+                    value = true
+                }
+            }
+        }
+    }
+    return value
+}
+
+// TEST CASES
+console.log(checkAB('lane borrowed')); // true
+console.log(checkAB('i am sick')); // false
+console.log(checkAB('you are boring')); // true
+console.log(checkAB('barbarian')); // true
+console.log(checkAB('bacon and meat')); // false
